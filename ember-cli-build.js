b629b3c67c20e84ba3909887a3d339361cdddad7
Paweł <jembezmamy@users.noreply.github.com>
2018-11-22 20:09:06 +0100

    initial modes implementation

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -10,7 +10,12 @@ module.exports = function(defaults) {
     visualTest: {
       imageLogging: true,
       debugLogging: true,
-      imgurClientId: '6331cc0a93af83c'
+      imgurClientId: '6331cc0a93af83c',
+      modes: {
+        mobile: {
+          windowWidth: 412
+        }
+      }
     },
     fingerprint: {
       exclude: [
