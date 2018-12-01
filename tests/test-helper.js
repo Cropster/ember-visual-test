e20b388438605409ad44da8c7780e735c1b20f68
Paweł <jembezmamy@users.noreply.github.com>
2018-12-01 14:06:03 +0100

    introducing 'skip visual tests' QUnit option

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -2,7 +2,9 @@ import Application from '../app';
 import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
+import setupVisualTests from 'ember-visual-test/test-support/setup';
 
 setApplication(Application.create(config.APP));
 
+setupVisualTests();
 start();
